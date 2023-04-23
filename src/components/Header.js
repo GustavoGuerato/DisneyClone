99bf8e59dc501060e0d056805f857feb04d7526a
GustavoGuerato <guerato.gustavo@gmail.com>
2023-04-23 17:26:40 -0300

    firebase setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -77,6 +77,29 @@ const NavMenu = styled.div`
       white-space: nowrap;
       position: relative;
     }
+
+    &:before {
+    background-color: rgb(249, 249, 249);
+    border-radius: 0px 0px 4px 4px;
+    bottom: -6px;
+    content: "";
+    height: 2px;
+    left: 0px;
+    opacity: 0;
+    position: absolute;
+    right: 0px;
+    transform-origin: left center;
+    transform: scaleX(0);
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+    visibility: hidden;
+    width: auto;
+  }
+  &:hover {
+    span:before {
+      transform: scaleX(1);
+      visibility: visible;
+      opacity: 1 important;
+    }
   }
 
   /* @media (max-width: 768px) {
