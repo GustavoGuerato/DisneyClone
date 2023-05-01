77f7db4ac5dc940423a0d32424d644c668437d04
GustavoGuerato <guerato.gustavo@gmail.com>
2023-05-01 20:59:20 -0300

    creation of recommends

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -4,33 +4,36 @@ const Viewers = (props) => {
   return (
     <Container>
       <Wrap>
-        <img src="/images/viewers-disney.png" alt="disney viewers" />
-        <video autoplay={true} loop={true} playsInline={true}>
+        <img src="/images/viewers-disney.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
           <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
         </video>
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-pixar.png" alt="pixar viewers" />
-        <video autoplay={true} loop={true} playsInline={true}>
+        <img src="/images/viewers-pixar.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
           <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
         </video>
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-marvel.png" alt="marvel viewers" />
-        <video autoplay={true} loop={true} playsInline={true}>
+        <img src="/images/viewers-marvel.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
           <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
         </video>
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-starwars.png" alt="starwars viewers" />
-        <video autoplay={true} loop={true} playsInline={true}>
+        <img src="/images/viewers-starwars.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
           <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
         </video>
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-national.png" alt="national viewers" />
-        <video autoplay={true} loop={true} playsInline={true}>
-          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+        <img src="/images/viewers-national.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source
+            src="/videos/1564676296-national-geographic.mp4"
+            type="video/mp4"
+          />
         </video>
       </Wrap>
     </Container>
@@ -53,7 +56,8 @@ const Container = styled.div`
 const Wrap = styled.div`
   padding-top: 56.25%;
   border-radius: 10px;
-  box-shadow: rgb(0 0 0/69) 0px 26px 30px -10px, rgb(0 0 0/73) 0 16px 10px -10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   cursor: pointer;
   overflow: hidden;
   position: relative;
