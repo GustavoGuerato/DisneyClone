569ab3b6f05d328fd4cb5060b9278c39e72d776c
GustavoGuerato <guerato.gustavo@gmail.com>
2023-05-06 18:25:58 -0300

    Add to Firebase & Display Movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable default-case */
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
@@ -5,8 +6,49 @@ import Recommends from "./Recommends";
 import Newdisney from "./NewDisney";
 import Originals from "./Originals";
 import Trending from "./Trending";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/user/userSlice";
 
 const Home = (props) => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    db.collection("movies").onSnapshot((snapshot) => {
+      snapshot.docs.map((doc) => {
+        switch (doc.data().type) {
+          case "recommend":
+            recommends.push({ id: doc.id, ...doc.data() });
+            break;
+          case "new":
+            newDisneys.push({ id: doc.id, ...doc.data() });
+            break;
+          case "original":
+            originals.push({ id: doc.id, ...doc.data() });
+            break;
+          case "trending":
+            trending.push({ id: doc.id, ...doc.data() });
+            break;
+        }
+      });
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisneys,
+          original: originals,
+          trending: trending,
+        })
+      );
+    });
+  }, [userName]);
+
   return (
     <Container>
       <ImgSlider />
